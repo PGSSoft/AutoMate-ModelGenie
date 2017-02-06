0a1eaba6a0e4b43aea5952eb424f85dad8941499
Ewelina Cyło <ecylo@pgs-soft.com>
2017-02-06 13:24:30 +0100

    Initial Commit

---
+++ ModelGenerator/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  ModelGenerator
+//
+//  Created by Pawel Szot on 12/08/16.
+//  Copyright © 2016 PSZOT. All rights reserved.
+//
+
+import Foundation
+
+let outputDirectory = "GeneratedModels"
+
+generateCountries()
+generateKeyboards()
+generateLabels()
+
+//generateLanguages() // Doesn't work with Xcode 8
